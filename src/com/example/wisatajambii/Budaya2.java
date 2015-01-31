6a24d36ebae6ad62556f88071b5f8f710d09f15a
faisal <faisalfikriamrullah@gmail.com>
2015-02-01 01:27:37 +0700

    commit pertama

---
+++ src/com/example/wisatajambii/Budaya2.java
@@ -0,0 +1,41 @@
+package com.example.wisatajambii;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Budaya2 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_budaya2);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.budaya2, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	public void back(View v){
+		Intent intentsaya=new Intent(getApplicationContext(),Budaya.class);		
+		startActivity(intentsaya);
+
+	}
+}
